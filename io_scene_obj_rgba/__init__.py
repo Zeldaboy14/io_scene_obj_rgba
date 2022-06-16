581488e05074421bf4942acef3fb08340ca7e9aa
Zeldaboy14 <55566323+Zeldaboy14@users.noreply.github.com>
2022-06-16 06:31:01 -0500

    updates
    allows installed from the file now

--- io_scene_obj_rgba/__init__.py
+++ io_scene_obj_rgba/__init__.py
@@ -19,16 +19,16 @@
 # <pep8-80 compliant>
 
 bl_info = {
-    "name": "Wavefront OBJ format",
-    "author": "Campbell Barton, Bastien Montagne",
+    "name": "Wavefront RGBA OBJ format ",
+    "author": "Campbell Barton, Bastien Montagne, modified by Z64me (z64.com)",
     "version": (2, 3, 7),
     "blender": (2, 78, 0),
     "location": "File > Import-Export",
-    "description": "Import-Export OBJ, Import OBJ mesh, UV's, materials and textures",
+    "description": "Import OBJ from Wumbas Wigwam/DK64 Viewer",
     "warning": "",
-    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Import-Export/Wavefront_OBJ",
+    "wiki_url": "https://github.com/RareExports/io_scene_obj_rgba",
     "support": 'OFFICIAL',
-    "category": "Import-Export"}
+    "category": "Import"}
 
 if "bpy" in locals():
     import importlib
@@ -174,155 +174,16 @@ class ImportOBJ(bpy.types.Operator, ImportHelper, IOOBJOrientationHelper):
         layout.prop(self, "use_image_search")
 
 
-class ExportOBJ(bpy.types.Operator, ExportHelper, IOOBJOrientationHelper):
-    """Save a Wavefront OBJ File"""
-
-    bl_idname = "export_scene.obj"
-    bl_label = 'Export OBJ'
-    bl_options = {'PRESET'}
-
-    filename_ext = ".obj"
-    filter_glob = StringProperty(
-            default="*.obj;*.mtl",
-            options={'HIDDEN'},
-            )
-
-    # context group
-    use_selection = BoolProperty(
-            name="Selection Only",
-            description="Export selected objects only",
-            default=False,
-            )
-    use_animation = BoolProperty(
-            name="Animation",
-            description="Write out an OBJ for each frame",
-            default=False,
-            )
-
-    # object group
-    use_mesh_modifiers = BoolProperty(
-            name="Apply Modifiers",
-            description="Apply modifiers",
-            default=True,
-            )
-    use_mesh_modifiers_render = BoolProperty(
-            name="Use Modifiers Render Settings",
-            description="Use render settings when applying modifiers to mesh objects",
-            default=False,
-            )
-
-    # extra data group
-    use_edges = BoolProperty(
-            name="Include Edges",
-            description="",
-            default=True,
-            )
-    use_smooth_groups = BoolProperty(
-            name="Smooth Groups",
-            description="Write sharp edges as smooth groups",
-            default=False,
-            )
-    use_smooth_groups_bitflags = BoolProperty(
-            name="Bitflag Smooth Groups",
-            description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
-                        "(produces at most 32 different smooth groups, usually much less)",
-            default=False,
-            )
-    use_normals = BoolProperty(
-            name="Write Normals",
-            description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
-            default=True,
-            )
-    use_uvs = BoolProperty(
-            name="Include UVs",
-            description="Write out the active UV coordinates",
-            default=True,
-            )
-    use_materials = BoolProperty(
-            name="Write Materials",
-            description="Write out the MTL file",
-            default=True,
-            )
-    use_triangles = BoolProperty(
-            name="Triangulate Faces",
-            description="Convert all faces to triangles",
-            default=False,
-            )
-    use_nurbs = BoolProperty(
-            name="Write Nurbs",
-            description="Write nurbs curves as OBJ nurbs rather than "
-                        "converting to geometry",
-            default=False,
-            )
-    use_vertex_groups = BoolProperty(
-            name="Polygroups",
-            description="",
-            default=False,
-            )
-
-    # grouping group
-    use_blen_objects = BoolProperty(
-            name="Objects as OBJ Objects",
-            description="",
-            default=True,
-            )
-    group_by_object = BoolProperty(
-            name="Objects as OBJ Groups ",
-            description="",
-            default=False,
-            )
-    group_by_material = BoolProperty(
-            name="Material Groups",
-            description="",
-            default=False,
-            )
-    keep_vertex_order = BoolProperty(
-            name="Keep Vertex Order",
-            description="",
-            default=False,
-            )
-
-    global_scale = FloatProperty(
-            name="Scale",
-            min=0.01, max=1000.0,
-            default=1.0,
-            )
-
-    path_mode = path_reference_mode
-
-    check_extension = True
-
-    def execute(self, context):
-        from . import export_obj
-
-        from mathutils import Matrix
-        keywords = self.as_keywords(ignore=("axis_forward",
-                                            "axis_up",
-                                            "global_scale",
-                                            "check_existing",
-                                            "filter_glob",
-                                            ))
-
-        global_matrix = (Matrix.Scale(self.global_scale, 4) *
-                         axis_conversion(to_forward=self.axis_forward,
-                                         to_up=self.axis_up,
-                                         ).to_4x4())
-
-        keywords["global_matrix"] = global_matrix
-        return export_obj.save(context, **keywords)
-
-
 def menu_func_import(self, context):
-    self.layout.operator(ImportOBJ.bl_idname, text="Wavefront (.obj)")
+    self.layout.operator(ImportOBJ.bl_idname, text="Wavefront RGBA (.obj)")
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportOBJ.bl_idname, text="Wavefront (.obj)")
+    self.layout.operator(ExportOBJ.bl_idname, text="Wavefront RGBA (.obj)")
 
 
 classes = (
     ImportOBJ,
-    ExportOBJ,
 )
 
 
@@ -331,12 +192,10 @@ def register():
         bpy.utils.register_class(cls)
 
     bpy.types.INFO_MT_file_import.append(menu_func_import)
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
     bpy.types.INFO_MT_file_import.remove(menu_func_import)
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
     for cls in classes:
         bpy.utils.unregister_class(cls)
